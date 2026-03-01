3a98f6b1f1213fc8e4952901ba5698b22ffb2ff7
Arun Rachuri <arunrachuri@Aruns-MacBook-Air.local>
2026-03-01 10:19:56 +0530

    ci: disable lint and ts build checks for Vercel

--- next.config.js
+++ next.config.js
@@ -1,1 +1,1 @@
-module.exports = { eslint: { ignoreDuringBuilds: true }, typescript: { ignoreBuildErrors: true } }
+module.exports = { eslint: { ignoreDuringBuilds: true }, typescript: { ignoreBuildErrors: true } };
